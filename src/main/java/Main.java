22f2b6d8b19a434acd3ee1542627dd6a6f3089ef
d.tkachenko <d.tkachenko@argustelecom.ru>
2023-12-11 22:47:09 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
         try (Connection connection =
-                DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "qwerty");)
-        {
+                DriverManager.getConnection("jdbc:postgresql://localhost:5433/postgres",
+                        "postgres", "qwerty")) {
+
             Statement statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS abc (id int)");
-            ResultSet resultSet = statement.executeQuery("SELECT * from car");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM car");
 
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
@@ -23,12 +23,9 @@ public class Main {
             }
 
             System.out.println("Подключено");
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
-
 
 
     }
